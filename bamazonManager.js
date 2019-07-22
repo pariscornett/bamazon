d43678c496d138c7097b380aa1d4bd2b11abaaa3
Paris Cornett <pariscornett@gmail.com>
2019-07-22 00:59:42 -0500

    added conditional statements that correspond with inquirer checkbox selection

--- bamazonManager.js
+++ bamazonManager.js
@@ -41,7 +41,40 @@ function manage(){
             ]
         }
     ]).then(function(selection){
+        var selection = selection.options;
         console.log(selection);
+        if (selection == "View products for sale"){
+            viewProducts();
+        }
+        else if(selection == "View low inventory"){
+            lowInventory();
+        }
+        else if (selection == "Add to inventory"){
+            addInventory();
+        }
+        else if (selection == "Add new product"){
+            addProduct();
+        }  
     })
 }
+
+//function to view products
+function viewProducts(){
+    console.log("\n Products currently available: \n");
+};
+
+//function to view low inventory
+function lowInventory(){
+    console.log("\n These items are low in stock \n");
+};
+
+//function to add inventory
+function addInventory(){
+    console.log("\n You're about to add inventory \n");
+};
+
+//function to add a new product
+function addProduct(){
+    console.log("\n You're about to add a new product \n");
+};
 
